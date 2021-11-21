623ece51913a8b9c47e847b7290a30ffb978973e
rifandyzv <rifandy2@gmail.com>
2021-11-22 00:23:18 +0700

    ??

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,8 @@ import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:123@127.0.0.1:8080/user")
+# engine = sqlalchemy.create_engine("postgresql://admin:admin@localhost:5432/user")
+engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:123@127.0.0.1:3306/user")
 Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
