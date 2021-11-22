c21766f333c4ea96550017fa26577272d0e46bba
rifandyzv <rifandy2@gmail.com>
2021-11-22 10:13:43 +0700

    fix database connection and docker-compose

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # engine = sqlalchemy.create_engine("postgresql://admin:admin@localhost:5432/user")
-engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:123@127.0.0.1:3306/user")
+engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:123@db-mariadb:3306/user")
 Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
