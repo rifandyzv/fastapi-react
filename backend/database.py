b5c175bd7be18d91dd5c860d4b22867aad08bc7d
rifandyzv <rifandy2@gmail.com>
2022-06-08 16:54:47 +0700

    commit database string

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # engine = sqlalchemy.create_engine("postgresql://admin:admin@localhost:5432/user")
-engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:123@db-mariadb:3306/user")
+# engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root:123@db-mariadb:3306/user")
+engine = sqlalchemy.create_engine("mariadb+mariadbconnector://root@localhost:3306/user")
+# engine = sqlalchemy.create_engine("postgresql://xpjfchbo:urdE0vW2Q8Q9iVYuX6-DGmundndpKKBc@rosie.db.elephantsql.com/xpjfchbo")
 Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
